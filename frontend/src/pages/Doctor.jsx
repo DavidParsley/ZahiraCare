234598adb35b07ceb189514b7ac9e21427553067
David Parsley <davidparsley.kakhayanga@gmail.com>
2024-12-19 00:19:41 +0300

    Adds add Doctor modal in Doctor Component

--- frontend/src/pages/Doctor.jsx
+++ frontend/src/pages/Doctor.jsx
@@ -4,6 +4,13 @@ import { Link } from "react-router-dom";
 export default function Doctor() {
   const [doctors, setDoctors] = useState([]);
 
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [department, setDepartment] = useState("");
+  const [specialization, setSpecialization] = useState("");
+  const [on_duty, setOn_duty] = useState(false);
+
   useEffect(() => {
     fetch("http://127.0.0.1:8000/doctors", {
       method: "GET",
@@ -13,12 +20,162 @@ export default function Doctor() {
       .catch((error) => console.log(error));
   }, []);
 
+  function handleAddDoctor(e) {
+    e.preventDefault();
+    fetch(`http://127.0.0.1:8000/doctors`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        email,
+        phone,
+        department,
+        specialization,
+        on_duty,
+      }),
+    })
+      .then((res) => res.json()) // Parse the response
+      .then((newDoctor) => {
+        setDoctors([...doctors, newDoctor]);
+      })
+      .then(() => nav("/doctors")) // Navigate to /patients page
+      .catch((error) => console.error("Error adding Doctor:", error));
+  }
+
   return (
     <main className="doctor-container">
+      <button
+        className="btn  add-patient-btn"
+        data-bs-toggle="modal"
+        data-bs-target="#patientModal"
+        style={{ color: "rgb(248,240,240)", backgroundColor: "rgb(145,5,8)" }}
+      >
+        Add New Doctor
+      </button>
+
+      {/* Add Doctor Modal*/}
+      <div
+        className="modal fade"
+        id="patientModal"
+        tabIndex="-1"
+        aria-labelledby="patientModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="patientModalLabel">
+                Add New Doctor
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <form onSubmit={handleAddDoctor}>
+              <div className="modal-body">
+                <div className="form-group">
+                  <label>Name</label>
+                  <input
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    className="form-control"
+                    placeholder="Enter Name"
+                    required 
+                  />
+                </div>
+
+                <div className="form-group">
+                  <label>Email</label>
+                  <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="form-control"
+                    placeholder="Enter Email"
+                    required 
+                  />
+                </div>
+
+                <div className="form-group">
+                  <label>Phone</label>
+                  <input
+                    type="text"
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
+                    className="form-control"
+                    placeholder="Enter Phone"
+                    required 
+                  />
+                </div>
+                <div className="form-group">
+                  <label>Department</label>
+                  <input
+                    type="text"
+                    value={department}
+                    onChange={(e) => setDepartment(e.target.value)}
+                    className="form-control"
+                    placeholder="Enter Department"
+                    required 
+                  />
+                </div>
+
+                <div className="form-group">
+                  <label>Specialization</label>
+                  <input
+                    type="text"
+                    value={specialization}
+                    onChange={(e) => setSpecialization(e.target.value)}
+                    className="form-control"
+                    placeholder="Enter Specialization"
+                    required 
+                  />
+                </div>
+                <div className="mb-3">
+                  <label>Status</label>
+                  <input
+                    type="checkbox"
+                    checked={on_duty}
+                    onChange={(e) => setOn_duty(e.target.checked)}
+                    className="form-check-input"
+                    style={{ marginLeft: '10px' }}
+                  />
+                </div>
+              </div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  data-bs-dismiss="modal"
+                >
+                  Close
+                </button>
+                <button
+                  type="submit"
+                  className="btn"
+                  style={{
+                    color: "rgb(248,240,240)",
+                    backgroundColor: "rgb(145,5,8)",
+                  }}
+                >
+                  Add Patient
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+
+      {/* This where the doctors fromthe database are being rendered */}
       <div className="doctor-cards">
         {doctors.map((doctor) => (
           <div className="doctor-card" key={doctor.id}>
-            <Link to={`/individualdoctor/${doctor.id}`}  className="card-link">
+            <Link to={`/individualdoctor/${doctor.id}`} className="card-link">
               <div className="profile">
                 <img
                   className="profile-img"
