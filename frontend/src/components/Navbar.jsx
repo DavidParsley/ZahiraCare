9dcdb6165d89946605cc416f6cf1b291cd6a25be
David Parsley <davidparsley.kakhayanga@gmail.com>
2024-12-19 01:13:50 +0300

    Adds svg in navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,14 +5,16 @@ export default function Navbar() {
     <div>
       <nav className="navbar navbar-expand-lg">
         <div className="container-fluid">
-          <a
-            className="navbar-brand"
-            href="#"
-            style={{
-              fontWeight: "bolder", // Make the brand text bolder
-            }}
-          >
-            ????
+          <a className="navbar-brand" href="#" style={{ fontWeight: "bolder" }}>
+            <svg 
+              xmlns="http://www.w3.org/2000/svg"
+              height="56px"
+              width="56px"
+              viewBox="0 -960 960 960"
+              fill="rgb(145,5,8)"
+            >
+              <path d="M420-340h120v-100h100v-120H540v-100H420v100H320v120h100v100Zm60 260q-139-35-229.5-159.5T160-516v-244l320-120 320 120v244q0 152-90.5 276.5T480-80Zm0-84q104-33 172-132t68-220v-189l-240-90-240 90v189q0 121 68 220t172 132Zm0-316Z" />
+            </svg>
           </a>
           <button
             className="navbar-toggler"
@@ -26,16 +28,10 @@ export default function Navbar() {
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            {/* Centering navbar links by using mx-auto on the ul */}
             <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 <a className="nav-link active" aria-current="page" href="/">
                   Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/about">
-                  About
                 </a>
               </li>
               <li className="nav-item">
@@ -61,7 +57,14 @@ export default function Navbar() {
                 placeholder="Search"
                 aria-label="Search"
               />
-              <button className="btn btn-outline" type="submit" style={{ color:  'rgb(248,240,240)', backgroundColor: 'rgb(145,5,8)',  }}>
+              <button
+                className="btn btn-outline"
+                type="submit"
+                style={{
+                  color: "rgb(248,240,240)",
+                  backgroundColor: "rgb(145,5,8)",
+                }}
+              >
                 Search
               </button>
             </form>
