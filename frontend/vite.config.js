30a20d8e619a147f30f507a692e76a32787745c2
David Parsley <davidparsley.kakhayanga@gmail.com>
2024-12-19 21:38:25 +0300

    vite config changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    historyApiFallback: true, // Ensures React Router works with history-based routing
+  },
+  base: '/',  // Adjust this if you're deploying to a subpath (e.g., /frontend/)
 })
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
