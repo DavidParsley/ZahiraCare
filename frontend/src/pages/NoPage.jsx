1a869cce9c74a994bdd8e8b935d0fbb62bd33b9a
David Parsley <davidparsley.kakhayanga@gmail.com>
2024-12-19 03:17:55 +0300

    Populates the No Page with information

--- frontend/src/pages/NoPage.jsx
+++ frontend/src/pages/NoPage.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from "react";
+
 
 export default function NoPage() {
   return (
-    <div>NoPage</div>
-  )
+    <div className="no-page-container">
+      <div className="no-page-content">
+        <h1>404</h1>
+        <p>The page you are trying to access doesn't exist.</p>
+        <a href={"/"} className="back-home">Go Back to Home</a>
+      </div>
+    </div>
+  );
 }
